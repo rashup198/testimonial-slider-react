1730543c97e2601f01a3cbc2ad76475efd304f0a
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-05 21:27:49 +0530

    updated card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,19 +11,18 @@ const Card = (props) => {
     <div className='flex flex-col md:relative'>
       <div className='absolute top-[-7rem] z-[10] mx-auto'>
         <img className='aspect-square rounded-full w-[140px] h-[140px] z-25' src={review.image}></img>
-        <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-20] left-[10px]'>
+        <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-3px] z-[-20] left-[5px]'>
         </div>
       </div>
 
       <div className='text-center mt-7' >
         <p className='font-bold text-2xl capitalize'>{review.name}</p>
-      </div>
-
-      <div className='text-center mt-7'  >
         <p className='text-violet-300 uppercase text-sm'>
             {review.job}
         </p>
       </div>
+
+      
 
       <div className=' text-violet-400 mx-auto mt-5'>
         <FaQuoteLeft></FaQuoteLeft>
