e2609b7ed53f1add8fb7aadecf5d3eb90cf80ca1
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-05 20:39:31 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import reviews from "./data"
 import Testimonial from "./components/Testimonial";
 
 const App = () => {
-  return <div>
-    <div>
-      <h1>Our Testimonials</h1>
-      <div></div>
+  return <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+    <div className="text-center">
+      <h1 className="text-4xl font-bold">Our Testimonials</h1>
+      <div className="bg-violet-400 h-[4px] w-[1/5]mt-1"></div>
       <Testimonial reviews={reviews}></Testimonial>
     </div>
   </div>;
